c220715c57a49ed7bba4bc63e824bb0bbc9e11ad
GiorgioGhortlishvili <56062823+giorgiocc@users.noreply.github.com>
2024-04-09 16:20:17 +0400

    Update settings.py

--- capstoneTech1/settings.py
+++ capstoneTech1/settings.py
@@ -12,18 +12,15 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-0(tfzr%d7_7q@_6%0i^lv7rp-#y9upe_dnmm@5rm++vhck_2sl"
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+
+
 
 ALLOWED_HOSTS = ["*"]
 
@@ -115,6 +112,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
